c78131542a0a1a0b06941ca03c7a9f16941cd30e
Matias Grunberg <matias@yellowspot.dev>
2025-01-03 11:33:16 -0300

    use a local variable for work queue

--- lib/sidekiq/heroku_autoscale/poll_interval.rb
+++ lib/sidekiq/heroku_autoscale/poll_interval.rb
@@ -34,21 +34,22 @@ module Sidekiq
           while @requests.size > 0
             ::Sidekiq.logger.warn "Polling #{@method_name} processes. Waiting for the semaphore."
             # Copy the requests, let the main thread continue to add new requests
+            @work = nil
             @semaphore.synchronize do
               ::Sidekiq.logger.warn "Polling #{@method_name} processes. Moving stuff to work queue."
-              work = @requests.dup
+              @work = @requests.dup
               ::Sidekiq.logger.warn "Polling #{@method_name} processes. Cleaning request queue."
-              work.each { |name, _process| @requests.delete(name) }
-              ::Sidekiq.logger.warn "Polling #{@method_name} processes. Request size after cleaning #{@requests.size}."
+              @work.each { |name, _process| @requests.delete(name) }
+              ::Sidekiq.logger.warn "Polling #{@method_name} processes. Request size after cleaning #{@requests.size}"
             end
 
             ::Sidekiq.logger.warn "Polling #{@method_name} processes. About to start iterating over work."
-            ::Sidekiq.logger.warn "Polling #{@method_name} processes. Work size #{work.size}."
+            ::Sidekiq.logger.warn "Polling #{@method_name} processes. Work size #{@work.size}."
 
-            while work.size > 0
-              ::Sidekiq.logger.warn "Polling #{@method_name} processes. Working - remaingin #{work.size}."
+            while @work.size > 0
+              ::Sidekiq.logger.warn "Polling #{@method_name} processes. Working - remaingin #{@work.size}."
               sleep(@before_update) if @before_update > 0
-              work.reject! { |n, p| p.send(@method_name) }
+              @work.reject! { |n, p| p.send(@method_name) }
               sleep(@after_update) if @after_update > 0
             end
             ::Sidekiq.logger.warn "Polling #{@method_name} processes. Work done. Request queued after working: #{@requests.size}"
